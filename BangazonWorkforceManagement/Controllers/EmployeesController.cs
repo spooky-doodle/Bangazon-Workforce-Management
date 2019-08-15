4309a7405b07deb36b5a9060f0f262806cd7dfe4
Colin Sandlin <csandlin010@gmail.com>
2019-08-15 12:10:48 -0700

    finished ticket #1 - HR can view all employees, including department name

---
+++ BangazonWorkforceManagement/Controllers/EmployeesController.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using BangazonAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace BangazonWorkforceManagement.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly IConfiguration _config;
+
+        public EmployeesController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        // GET: Employee
+        public ActionResult Index()
+        {
+            var employees = GetAllEmployees();
+            return View(employees);
+        }
+
+        // GET: Employee/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Employee/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employee/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Employee/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Employee/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Employee/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Employee/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        private List<Employee> GetAllEmployees()
+        {
+            var employees = new List<Employee>();
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+
+                using(SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                                        SELECT e.Id, e.FirstName, e.LastName, e.DepartmentId, d.Name
+                                        FROM Employee e
+                                        LEFT JOIN Department d
+                                        on d.Id = e.DepartmentId
+                                      ";
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while(reader.Read())
+                    {
+                        employees.Add(new Employee()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                            LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                            DepartmentId = reader.GetInt32(reader.GetOrdinal("DepartmentId")),
+                            Department = 
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                Name = reader.GetString(reader.GetOrdinal("Name"))
+                            }
+                    });
+                    }
+                    reader.Close();
+                }
+            }
+            return employees;
+        }
+    }
+}
--- BangazonWorkforceManagement/Models/Employee.cs
+++ BangazonWorkforceManagement/Models/Employee.cs
@@ -8,19 +8,46 @@ namespace BangazonAPI.Models
 {
     public class Employee
     {
+
+        //Given an HR employee wants to view employees
+        //When the employee clicks on the Employees item in the navigation bar
+        //Then all current employees should be listed with the following information
+
+        //1. First name and last name
+        //2. Department
+
+        [Required]
+        [Display(Name = "Employee Id")]
         public int Id { get; set; }
+
         [Required]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+
         [Required]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+
         [Required]
+        [Display(Name = "Department Id")]
         public int DepartmentId { get; set; }
+
         [Required]
         public bool IsSupervisor { get; set; }
 
+        [Display(Name = "Department Name")]
         public Department Department = new Department();
 
         public List<Computer> Computers = new List<Computer>();
 
+        [Display(Name = "Full Name")]
+        public string FullName
+
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
     }
 }
