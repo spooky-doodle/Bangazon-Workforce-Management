9b2712b7b75c6fa3beb8e8507a8400965ef1c4ad
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-08-15 12:31:04 -0500

    Added models

---
+++ BangazonWorkforceManagement/Models/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonAPI.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public int DepartmentId { get; set; }
+        [Required]
+        public bool IsSupervisor { get; set; }
+
+        public Department Department = new Department();
+
+        public List<Computer> Computers = new List<Computer>();
+
+    }
+}
---
+++ BangazonWorkforceManagement/Models/TrainingProgram.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonAPI.Models
+{
+    public class TrainingProgram
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public DateTime StartDate { get; set; }
+        [Required]
+        public DateTime EndDate { get; set; }
+        [Required]
+        public int MaxAttendees { get; set; }
+
+        public List<Employee> Attendees { get; set; }
+
+    }
+}
